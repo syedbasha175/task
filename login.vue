b6e6631cc7febfe07895e2ab486cd1e0020bebe4
syedbasha175 <51587504+syedbasha175@users.noreply.github.com>
2019-06-10 12:38:51 +0530

    Add files via upload

---
+++ login.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+
+<h4 >Username:<input type="text"  v-model="username" id="txt"/></h4>
+<h4 >Mail ID:<input type="text"  v-model="usermail" id="mail"/></h4>
+
+<input type="submit" value="click" v-on:click="passvalues();"/>
+
+
+</div>
+</template>
+<script>
+
+export default {
+     
+data() {
+  
+    return {
+      
+            username: "",
+            usermail:"",
+          
+        }
+    },
+    methods:{
+          passvalues()  {
+  this.$router.push({ path: '/mycomp?name='+this.username } );
+}
+    }
+  
+}
+        //  function passvalues(){
+        // {
+        //     var firstname=document.getElementById("txt").value;
+        //     localStorge.setItem("textvalue",firstname);
+        //     return false;
+        // }
+
+</script>
---
+++ mycomponent.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<span  v-model="name">Username is {{name}}</span>
+
+</div>
+</template>
+
+<script>
+
+export default {
+ 
+  data () {
+    return {
+      name : this.$route.query.name
+    }
+     
+    }
+  }
+</script>
